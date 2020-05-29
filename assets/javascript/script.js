badb06804b1fd8a05c378ebdefd704cb0111008e
Jean <jeanhern81@gmail.com>
2020-05-29 13:37:50 -0700

    added basic styling, background color, some bootstrap elements

---
+++ assets/javascript/script.js
@@ -0,0 +1,40 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      number: "Question One",
+      question: "for (var i = 0; i < letter.lenght; i++); is an example of what?",
+      choices: ["array", "alerts", "for-loop", "console.log"],
+      answer: "for-loop"
+    },
+    {
+      number: "Question Two",
+      question: "This Logical Operator || means ____.",
+      choices: ["and", "equalt to", "or", "not equal to"],
+      answer: "or"
+    },
+    {
+      number: "Question Three",
+      question: "In the following example, what food  would be found on index 2: var favoriteFood = ["tacos", "pizza", "goulash"].
+      choices: [
+        "steak",
+        "goulash",
+        "pizza",
+        "non of the above"
+      ],
+      answer: "goulash"
+    },
+    {
+      number: "Question Three",
+      question:
+        "Which are these is not a Comparison Operator?",
+      choices: ["===", "=>==", "!==", "<="],
+      answer: "=>=="
+    },
+    {
+      number: "Question Four",
+      question:
+        "Alerts, prompts, and confirms are examples of?",
+      choices: ["JavaScript popup box", "console.log", "for loops", "Math.random"],
+      answer: "JavaScript popup box"
+    }
+  ];
