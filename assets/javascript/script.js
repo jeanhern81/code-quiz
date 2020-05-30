34511c4abf0c0c744d86b87e98f7aed732c8d348
Jean <jeanhern81@gmail.com>
2020-05-30 15:20:00 -0700

    added my first question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,46 +1,55 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById
 ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-const shuffledQuestion, currentQuestionsIndx
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz)
 
 
-function startQuiz () {
+function startQuiz() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random () - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
-    setNextQuestion ()
+    setNextQuestion()
 
-};
+}
 
-function setNextQuestion () {
-
-
-
-};
-
-function selectAnswer () {
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 
+}
+function showQuestion(question) {
+    questionElement.innerText = question.question
 
 
-};
+}
 
 
-const question = [
-{
-    question: "for (var i = 0; i < letter.lenght; i++); is an example of what?",
-    choices: ["array", "alerts", "for-loop", "console.log"],
-    answer: "for-loop"
 
-
+function selectAnswer() {
 
 
 
 
 }
 
+
+const questions = [
+{
+    question: 'Which are the following is not a Comparison Operator?',
+    answers: [
+        { text: '===', correct: false },
+        { text: '=>==', correct: true },
+        { text: '!==', correct: false },
+        { text: '<=', correct: false },
+    
+    ]
+}
 
 ]
